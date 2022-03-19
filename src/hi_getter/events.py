c309b69e3618e9d05073e28e0ca1e5f34ea83162
Cubicpath <Cubicpath@pm.me>
2022-03-18 22:32:08 -0400

    add _EventBusMeta metaclass for global EventBus access

--- src/hi_getter/events.py
+++ src/hi_getter/events.py
@@ -12,7 +12,7 @@ __all__ = (
 from collections.abc import Callable
 from collections.abc import Generator
 from typing import Any
-from typing import Type
+from typing import Optional
 
 from .utils import get_parent_doc
 
@@ -88,20 +88,62 @@ class Event:
         return doc.splitlines()[0]
 
 
+class _EventBusMeta(type):
+    """Metaclass for :py:class:`EventBus`.
+
+    Maps :py:class:`EventBus` objects to :py:class:`str` ids.
+    """
+    _id_bus_map: dict[str, 'EventBus'] = {}
+
+    @classmethod
+    def __new__(mcs, *args, **kwargs) -> type['EventBus']:
+        clazz: type['EventBus'] = super().__new__(*args, **kwargs)
+        return clazz
+
+    @classmethod
+    def __getitem__(mcs, id: str) -> Optional['EventBus']:
+        return mcs._id_bus_map.get(id.lower())
+
+    @classmethod
+    def __setitem__(mcs, id: str, bus: 'EventBus') -> None:
+        """Set an :py:class:`EventBus` from the bus map."""
+        if not isinstance(id, str):
+            raise TypeError(f'parameter {id=} is not of type {str}.')
+        if not isinstance(bus, EventBus):
+            raise TypeError(f'parameter {bus=} is not of type {EventBus}.')
+        mcs._id_bus_map[id.lower()] = bus
+
+    @classmethod
+    def __delitem__(mcs, id: str) -> None:
+        """Delete an :py:class:`EventBus` from the bus map."""
+        del mcs._id_bus_map[id.lower()]
+
+
 # pylint: disable=deprecated-typing-alias
-class EventBus:
+class EventBus(metaclass=_EventBusMeta):
     """Object that keeps track of all :py:class:`Callable` subscriptions to :py:class:`Event`'s.
 
-    All eventbuses are stored in the class with a unique id.
+    All :py:class:`EventBus`'s are stored in the class with a unique id.
+    You can access created :py:class:`EventBus`'s with subscripts. ex::
+
+        EventBus['foo'] -> None
+        EventBus('foo') -> EventBus object at 0x000000001
+        EventBus['foo'] -> EventBus object at 0x000000001
+        del EventBus['foo']
+        EventBus['foo'] -> None
     """
 
-    def __init__(self) -> None:
-        self._event_subscribers: dict[Type[Event], list[tuple[
-            Callable[[Event], None] | Callable,  # Callable to run
-            Callable[[Event], bool] | None       # Optional predicate to run callable
-        ]]] = {}
+    def __init__(self, __id: str, /) -> None:
+        if type(self)[__id] is None:
+            self._event_subscribers: dict[type[Event], list[tuple[
+                Callable[[Event], None] | Callable,  # Callable to run
+                Callable[[Event], bool] | None       # Optional predicate to run callable
+            ]]] = {}
+            type(self)[__id] = self
+        else:
+            raise KeyError(f'EventBus id "{__id}" is already defined in {type(type(self)).__name__}')
 
-    def clear(self, event: Type[Event] | None = None) -> None:
+    def clear(self, event: type[Event] | None = None) -> None:
         """Clear event subscribers of a given type.
 
         None is treated as a wildcard and deletes ALL event subscribers.
@@ -113,7 +155,7 @@ class EventBus:
         else:
             self._event_subscribers.pop(event)
 
-    def fire(self, event: Event | Type[Event]) -> None:
+    def fire(self, event: Event | type[Event]) -> None:
         """Fire all :py:class:`Callables` subscribed to the :py:class:`Event`'s :py:class:`type`.
 
         :py:class:`Event` subclasses call their parent's callables as well.
@@ -142,7 +184,7 @@ class EventBus:
                         # Finally, call
                         e_callable_pair[0](*event_args)
 
-    def subscribe(self, __callable: Callable, /, event: Type[Event], event_predicate: Callable[[Event], bool] | None = None) -> None:
+    def subscribe(self, __callable: Callable, /, event: type[Event], event_predicate: Callable[[Event], bool] | None = None) -> None:
         """Subscribe a :py:class:`Callable` to an :py:class:`Event` type.
 
         By default, every time an :py:class:`Event` is fired, it will call the callable with the event as an argument.
