3ef2ec614221ad7ff3451b42f04e476a0f3e6d27
Cubicpath <Cubicpath@pm.me>
2022-04-08 02:16:19 -0400

    add ExceptionEvent

--- src/hi_getter/exceptions.py
+++ src/hi_getter/exceptions.py
@@ -4,14 +4,24 @@
 """Custom :py:class:`Exception`'s and excepthook implementation."""
 
 __all__ = (
+    'ExceptionEvent',
     'ExceptionHook',
 )
 
 import sys
-from types import BuiltinFunctionType
+from collections.abc import Callable
 from types import TracebackType
 
 from .events import *
+
+
+class ExceptionEvent(Event):
+    """Event fired when an exception is caught by an :py:class:`ExceptionHook`."""
+    __slots__ = ('exception', 'traceback')
+
+    def __init__(self, exception: BaseException, traceback: TracebackType) -> None:
+        self.exception: BaseException = exception
+        self.traceback: TracebackType = traceback
 
 
 class ExceptionHook:
@@ -19,16 +29,16 @@ class ExceptionHook:
 
     def __init__(self):
         """Initialize the :py:class:`ExceptionHook` for use in a context manager."""
-        self.__old_hook: BuiltinFunctionType | None = None
-        EventBus['exceptions'] = EventBus()
+        self.__old_hook: Callable[[type[BaseException], BaseException, TracebackType], None] | None = None
+        self.event_bus:  EventBus | None = None
 
     def __call__(self, type_: type[BaseException], exception: BaseException, traceback: TracebackType) -> None:
         """Called when an exception is raised."""
         if not issubclass(type_, Exception):
             # Don't handle BaseException's
-            return self.__old_hook(self, type_, exception, traceback)
+            return self.__old_hook(type_, exception, traceback)
 
-        print(f'{type_} raised: {exception}', file=sys.stderr)
+        EventBus['exceptions'] << ExceptionEvent(exception, traceback)
 
     def __repr__(self) -> str:
         return f'<{self.__class__.__name__} (old_hook={self.__old_hook})>'
@@ -36,11 +46,13 @@ class ExceptionHook:
     def __enter__(self) -> None:
         """Temporary extend current exception hook."""
         self.__old_hook = sys.excepthook
+        self.event_bus = EventBus('exceptions')
         sys.excepthook = self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         """Reset current exception hook to the original one."""
         sys.excepthook = self.__old_hook
+        del EventBus['exceptions']
 
     @property
     def old_hook(self):
