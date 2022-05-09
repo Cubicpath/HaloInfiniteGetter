7fa32d49bf4a36c241eed5ede0ef38e7ef580fde
Cubicpath <Cubicpath@pm.me>
2022-05-09 02:33:06 -0400

    allow __main__.py import support

--- src/hi_getter/__main__.py
+++ src/hi_getter/__main__.py
@@ -2,10 +2,19 @@
 #                              MIT Licence (C) 2022 Cubicpath@Github                              #
 ###################################################################################################
 """Entrypoint that is called when using Python's -m switch."""
+
+__all__ = (
+    'exit_code',
+    'main',
+)
+
 import sys
 
 from .run import main
 
 # Allow running from import, as __name__ should be __main__
 exit_code = main(*sys.argv)
-sys.exit(exit_code)
+
+# If imported, do not exit
+if '__main__' != __name__:
+    sys.exit(exit_code)
