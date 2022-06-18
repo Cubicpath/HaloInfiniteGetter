f8b0b995b80e0968b9bff9fdf75350c14c18ee6c
Cubicpath <Cubicpath@pm.me>
2022-06-17 22:21:35 -0400

    add url_params_to_query utility function

--- src/hi_getter/network/utils.py
+++ src/hi_getter/network/utils.py
@@ -7,11 +7,14 @@ __all__ = (
     'decode_url',
     'encode_url_params',
     'http_code_map',
+    'url_params_to_query',
 )
 
 from http import HTTPStatus
 from urllib.parse import unquote as decode_url
 from urllib.parse import urlencode as encode_url_params
+
+from PySide6.QtCore import *
 
 # pylint: disable=not-an-iterable
 http_code_map = {status.value: (status.phrase, status.description) for status in HTTPStatus}
@@ -25,3 +28,10 @@ for _code, _description in {
 }.items():
     http_code_map[_code] = (http_code_map[_code][0], _description)
     del _code, _description
+
+
+def url_params_to_query(params: dict[str, str]) -> QUrlQuery:
+    """Transforms a param name and value pair into a :py:class:`QUrlQuery` object."""
+    query = QUrlQuery()
+    query.setQueryItems([(key, value) for key, value in params.items()])
+    return query
