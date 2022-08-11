180231bf32dd08e48b7dca49dd9e54cd1cfeea12
Cubicpath <Cubicpath@pm.me>
2022-08-11 08:01:46 -0400

    hotfix for pypi, remove Client import in hi_getter/__init__.py

--- src/hi_getter/__init__.py
+++ src/hi_getter/__init__.py
@@ -3,14 +3,8 @@
 ###################################################################################################
 """A simple way to get live Halo data straight from Halo Waypoint."""
 # TODO: Add logging functionality
-
-__all__ = (
-    'Client',
-)
-
 from ._version import __version__
 from ._version import __version_info__
-from .network import Client
 
 __author__ = 'Cubicpath@Github <cubicpath@pm.me>'
 """Author's information."""
