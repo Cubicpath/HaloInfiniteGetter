b8e2b318aa9693faeda04a837ec9465f0fa79e23
Cubicpath <Cubicpath@pm.me>
2022-10-30 02:36:34 -0400

    add Import and Export workers

---
+++ src/hi_getter/gui/workers.py
@@ -0,0 +1,67 @@
+###################################################################################################
+#                              MIT Licence (C) 2022 Cubicpath@Github                              #
+###################################################################################################
+"""Implementations for worker-thread runnables."""
+import shutil
+from collections.abc import Callable
+from pathlib import Path
+
+from PySide6.QtCore import *
+
+__all__ = (
+    'ExportData',
+    'ImportData',
+)
+
+
+class _Worker(QRunnable):
+    def _run(self) -> None:
+        raise NotImplementedError
+
+    @Slot()
+    def run(self) -> None:
+        """Called by the :py:class:`QThreadPool`."""
+        self._run()
+
+
+class ExportData(_Worker):
+    """Build archive in a temp file then remove .tmp extension when finished."""
+    EXTENSION_FORMATS: dict[str, str] = {
+        '.7z': '7z',
+        '.tar': 'tar',
+        '.zip': 'zip', '.piz': 'zip',
+        '.tar.gz': 'gztar', '.tgz': 'gztar',
+        '.tar.bz2': 'bztar', '.tbz2': 'bztar',
+        '.tar.xz': 'xztar', '.txz': 'xztar'
+    }
+
+    def __init__(self, base: Path, dest: Path) -> None:
+        super().__init__()
+        self.base = base
+        self.dest = dest
+
+    def _run(self) -> None:
+        temp_archive_path: str = shutil.make_archive(
+            QDir.tempPath() + f'/{self.dest.stem}', self.EXTENSION_FORMATS.get(self.dest.suffix),
+            root_dir=self.base.parent, base_dir=f'./{self.base.name}'
+        )
+
+        shutil.move(
+            src=temp_archive_path,
+            dst=self.dest
+        )
+
+
+class ImportData(_Worker):
+    """Extract from archive into a directory."""
+    def __init__(self, archive: Path, dest: Path, on_error: Callable[[BaseException], None] | None = None) -> None:
+        super().__init__()
+        self.archive = archive
+        self.dest = dest
+        self.on_error = on_error
+
+    def _run(self) -> None:
+        try:
+            shutil.unpack_archive(str(self.archive), extract_dir=self.dest)
+        except (OSError, ValueError) as e:
+            self.on_error(e)
