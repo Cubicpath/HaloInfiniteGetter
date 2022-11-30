1b39c2b161c4758d174b146d895e1eddd6c78b2d
Cubicpath <Cubicpath@pm.me>
2022-11-30 00:19:42 -0500

    Add pyright to dev dependencies and pre-commit

--- src/hi_getter/__init__.py
+++ src/hi_getter/__init__.py
@@ -4,6 +4,7 @@
 """A simple way to get live Halo data straight from Halo Waypoint."""
 # TODO: Add logging functionality
 # TODO: Redo Events implementation
+# TODO: Remove pyright comments when PySide6 signals and events are properly type hinted.
 from ._version import __version__
 from ._version import __version_info__
 
