8d5aee4371f4836df2bcf181cde2748f7ef99777
Cubicpath <Cubicpath@pm.me>
2022-11-30 23:09:42 -0500

    Implement local vendor `packaging` package over setuptool's

--- src/hi_getter/network/version_check.py
+++ src/hi_getter/network/version_check.py
@@ -14,9 +14,11 @@ from datetime import datetime
 from importlib.metadata import version
 
 from PySide6.QtCore import *
-# noinspection PyProtectedMember, PyUnresolvedReferences
-from setuptools._vendor.packaging.version import Version
 
+from .._vendor.packaging import BaseVersion
+from .._vendor.packaging import LegacyVersion
+from .._vendor.packaging import parse_version
+from .._vendor.packaging import Version
 from .._version import __version__
 from ..constants import *
 from ..utils.package import has_package
@@ -24,18 +26,18 @@ from .manager import NetworkSession
 from .manager import Response
 
 
-def get_version(package_name: str) -> Version | None:
+def get_version(package_name: str) -> LegacyVersion | Version | None:
     """Return the :py:class:`Version` of the given package if it is installed. Else return None."""
     if has_package(package_name):
-        return Version(version(package_name))
+        return parse_version(version(package_name))
 
 
-def is_greater_version(version1: Version | str, version2: Version | str) -> bool:
+def is_greater_version(version1: BaseVersion | str, version2: BaseVersion | str) -> bool:
     """Return whether ``version1`` is greater than ``version2``."""
-    if not isinstance(version1, Version):
-        version1 = Version(version1)
-    if not isinstance(version2, Version):
-        version2 = Version(version2)
+    if not isinstance(version1, BaseVersion):
+        version1 = parse_version(version1)
+    if not isinstance(version2, BaseVersion):
+        version2 = parse_version(version2)
 
     return version1 > version2
 
@@ -70,10 +72,11 @@ class VersionChecker(QObject):
             )
 
             # Get local version of given package. Use __version__ attribute for own package.
-            local_version: Version | str
+            local_version: BaseVersion | str
             if package_name != HI_PACKAGE_NAME:
-                if (local_version := get_version(package_name)) is None:
+                if (ver := get_version(package_name)) is None:
                     return
+                local_version = ver
             else:
                 local_version = __version__
 
