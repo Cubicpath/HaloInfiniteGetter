33745f2d3afc662229c9df8b107c2b21a755effe
Cubicpath <Cubicpath@pm.me>
2023-02-22 12:48:04 -0500

    File dialog for selecting jsons to scan

--- src/hi_getter/gui/windows/scan_selector_dialog.py
+++ src/hi_getter/gui/windows/scan_selector_dialog.py
@@ -8,6 +8,7 @@ __all__ = (
     'ScanSelectorDialog',
 )
 
+from pathlib import Path
 
 from PySide6.QtCore import *
 from PySide6.QtGui import *
@@ -16,6 +17,13 @@ from PySide6.QtWidgets import *
 from ...utils import init_layouts
 from ...utils import init_objects
 from ..aliases import app
+from ..aliases import tr
+
+
+_SCAN_FILTER: str = ';;'.join((
+    'JSON Files (*.json)',
+    'All files (*.*)'
+))
 
 
 class ScanSelectorDialog(QWidget):
@@ -39,6 +47,10 @@ class ScanSelectorDialog(QWidget):
                 'windowIcon': self.style().standardIcon(QStyle.StandardPixmap.SP_FileDialogContentsView),
             },
 
+            (filepaths_selector := QPushButton(self)): {
+                'clicked': self.select_files
+            },
+
             self.scan_button: {
                 # 'clicked': start_sign_in,
             },
@@ -51,19 +63,24 @@ class ScanSelectorDialog(QWidget):
         app().init_translations({
             self.setWindowTitle: 'gui.scan_selector_dialog.title',
             self.filepaths_input.setPlaceholderText: 'gui.scan_selector_dialog.filepaths_placeholder',
+            filepaths_selector.setText: 'gui.scan_selector_dialog.select_files',
 
             self.scan_button.setText: 'gui.scan_selector_dialog.scan',
             cancel_button.setText: 'gui.scan_selector_dialog.cancel'
         })
 
         init_layouts({
+            (input_layout := QHBoxLayout()): {
+                'items': (self.filepaths_input, filepaths_selector)
+            },
+
             (bottom := QHBoxLayout()): {
                 'items': (self.scan_button, cancel_button)
             },
 
             # Main layout
             QVBoxLayout(self): {
-                'items': (self.filepaths_input, bottom)
+                'items': (input_layout, bottom)
             }
         })
 
@@ -72,7 +89,19 @@ class ScanSelectorDialog(QWidget):
 
         self.scan_button.setDisabled(True)
 
+    def select_files(self) -> None:
+        """Select JSON files to scan through a file dialog."""
+        names: list[str] = QFileDialog.getOpenFileNames(self, caption=tr('gui.scan_selector_dialog.select_files'),
+                                                        dir=str(Path.home() / 'Downloads'),
+                                                        filter=_SCAN_FILTER)[0]
+        # If no filenames are selected, do nothing.
+        if not names:
+            return
+
+        # Override input text with a string representation of selected files
+        self.filepaths_input.setText(', '.join(f'"{Path(file)}"' for file in names))
+
     def showEvent(self, event: QShowEvent) -> None:
-        """Reset fields when dialog is shown to user."""
+        """Reset widgets when dialog is shown to user."""
         super().showEvent(event)
         self._reset_widgets()
