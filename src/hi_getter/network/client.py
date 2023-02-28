6a21f939d62c909cff5ccacd3b632763892b8b69
Cubicpath <Cubicpath@pm.me>
2023-02-28 12:21:49 -0500

    Disable `Copy Endpoint Path` for cached resources that don't have an endpoint path

--- src/hi_getter/network/client.py
+++ src/hi_getter/network/client.py
@@ -337,8 +337,14 @@ class Client(QObject):
         return parent / self.sub_host.replace('-', '_') / self.parent_path.strip('/') / path
 
     def to_get_path(self, path: str) -> str:
-        """Translate a given cache location to the equivalent GET path."""
+        """Translate a given cache location to the equivalent GET path.
+
+        Return an empty string if path doesn't include an endpoint following the parent_path.
+        """
         resource = path.split(self.parent_path, maxsplit=1)[-1]
+        if Path(resource).is_absolute():
+            return ''
+
         return resource
 
     def refresh_auth(self) -> None:
